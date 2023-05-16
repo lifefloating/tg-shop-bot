464687338c6468510850c85de4ffe286c9b168cb
yangqiqi <ydy00444@edocyun.com.cn>
2023-05-16 17:56:01 +0800

    .

--- controller/api_worker.py
+++ controller/api_worker.py
@@ -91,7 +91,7 @@ class ApiWorker(object):
                 'product_id': item.product_id,
                 'product_name': item.product.name,
                 'product_price': item.product.price,
-                'product_image': base64.b64decode(item.product.image),
+                # 'product_image': base64.b64decode(item.product.image),
                 'quantity': item.quantity,
                 'amount': item.amount
             })
